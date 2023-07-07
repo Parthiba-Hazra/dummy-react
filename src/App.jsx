846df05890b24a3a30d085922b3dffbb1bffbe88
Parthiba-Hazra <parthibahazra02@gmail.com>
2023-07-07 21:36:21 +0530

    learning react
    Signed-off-by: Parthiba-Hazra <parthibahazra02@gmail.com>

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Post from "./components/Post";
+
+function App() {
+  return(
+    <main>
+      <Post author="Parthiba" body="React.js is awesome!" />
+      <Post author="Prarth" body="learning react"/>
+    </main>
+  );
+}
+
+export default App
